170c9894a6fcb6af7be19a93b37d17584c1ecb30
MS269 <59796004+MS269@users.noreply.github.com>
2021-05-15 18:18:47 +0900

    templates

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,12 +1,45 @@
-export const trending = (req, res) => res.send("Trending");
+const fakeUser = {
+  username: "Damien",
+  loggedIn: true,
+};
+
+export const trending = (req, res) => {
+  const fakeVideos = [
+    {
+      title: "First Video",
+      rating: 5,
+      comments: 2,
+      createdAt: "2 minutes ago",
+      views: 269,
+      id: 1,
+    },
+    {
+      title: "Second Video",
+      rating: 5,
+      comments: 2,
+      createdAt: "2 minutes ago",
+      views: 269,
+      id: 2,
+    },
+    {
+      title: "Third Video",
+      rating: 5,
+      comments: 2,
+      createdAt: "2 minutes ago",
+      views: 269,
+      id: 3,
+    },
+  ];
+  return res.render("home", {
+    pageTitle: "Home",
+    user: fakeUser,
+    videos: fakeVideos,
+  });
+};
 export const search = (req, res) => res.send("Search");
 export const upload = (req, res) => res.send("Upload");
-export const see = (req, res) => {
-  return res.send(`Video #${req.params.id} See`);
-};
-export const edit = (req, res) => {
-  return res.send(`Video #${req.params.id} Edit`);
-};
+export const see = (req, res) => res.render("watch", { pageTitle: "Watch" });
+export const edit = (req, res) => res.render("edit", { pageTitle: "Edit" });
 export const deleteVideo = (req, res) => {
   return res.send(`Video #${req.params.id} Delete Video`);
 };
