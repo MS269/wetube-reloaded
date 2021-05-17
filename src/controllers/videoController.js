3fa57c5146ba9f37a8cc1e05bce7e0046762f4bc
MS269 <59796004+MS269@users.noreply.github.com>
2021-05-18 00:21:34 +0900

    mongoDB and mongoose

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,45 +1,26 @@
-const fakeUser = {
-  username: "Damien",
-  loggedIn: true,
-};
+import Video from "../models/Video";
 
-export const trending = (req, res) => {
-  const fakeVideos = [
-    {
-      title: "First Video",
-      rating: 5,
-      comments: 2,
-      createdAt: "2 minutes ago",
-      views: 269,
-      id: 1,
-    },
-    {
-      title: "Second Video",
-      rating: 5,
-      comments: 2,
-      createdAt: "2 minutes ago",
-      views: 269,
-      id: 2,
-    },
-    {
-      title: "Third Video",
-      rating: 5,
-      comments: 2,
-      createdAt: "2 minutes ago",
-      views: 269,
-      id: 3,
-    },
-  ];
-  return res.render("home", {
-    pageTitle: "Home",
-    user: fakeUser,
-    videos: fakeVideos,
+export const home = (req, res) => {
+  Video.find({}, (error, videos) => {
+    return res.render("home", { pageTitle: "Home", videos });
   });
 };
-export const search = (req, res) => res.send("Search");
-export const upload = (req, res) => res.send("Upload");
-export const see = (req, res) => res.render("watch", { pageTitle: "Watch" });
-export const edit = (req, res) => res.render("edit", { pageTitle: "Edit" });
-export const deleteVideo = (req, res) => {
-  return res.send(`Video #${req.params.id} Delete Video`);
+export const watch = (req, res) => {
+  const { id } = req.params;
+  return res.render("watch", { pageTitle: `Watching:` });
+};
+export const getEdit = (req, res) => {
+  const { id } = req.params;
+  return res.render("edit", { pageTitle: `Editing:` });
+};
+export const postEdit = (req, res) => {
+  const { id } = req.params;
+  const { title } = req.body;
+  return res.redirect(`/videos/${id}`);
+};
+export const getUpload = (req, res) =>
+  res.render("upload", { pageTitle: "Upload Video" });
+export const postUpload = (req, res) => {
+  const { title } = req.body;
+  return res.redirect("/");
 };
