3bf1312a5e27cf26cd8361f35475823491df0b1d
MS269 <59796004+MS269@users.noreply.github.com>
2021-05-23 21:58:40 +0900

    user authentication

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,8 +1,69 @@
-export const join = (req, res) => res.send("Join");
-export const login = (req, res) => res.send("login");
-export const logout = (req, res) => res.send("Log out");
-export const edit = (req, res) => res.send("Edit User");
-export const deleteUser = (req, res) => res.send("Remove User");
-export const see = (req, res) => {
-  return res.send(`User #${req.params.id} See`);
+import User from "../models/User";
+import bycrypt from "bcrypt";
+
+export const getJoin = (req, res) => res.render("join", { pageTitle: "Join" });
+
+export const postJoin = async (req, res) => {
+  const { name, username, email, password, password2, location } = req.body;
+  const pageTitle = "Join";
+  if (password !== password2) {
+    return res.status(400).render("join", {
+      pageTitle,
+      errorMessage: "Password confirmation does not match.",
+    });
+  }
+  const exists = await User.exists({ $or: [{ username }, { email }] });
+  if (exists) {
+    return res.status(400).render("join", {
+      pageTitle,
+      errorMessage: "This username/email is already taken.",
+    });
+  }
+  try {
+    await User.create({
+      name,
+      username,
+      email,
+      password,
+      location,
+    });
+  } catch (error) {
+    return res.status(400).render("join", {
+      pageTitle,
+      errorMessage: error._message,
+    });
+  }
+  return res.redirect("/login");
 };
+
+export const getLogin = (req, res) =>
+  res.render("login", { pageTitle: "Login" });
+
+export const postLogin = async (req, res) => {
+  const { username, password } = req.body;
+  const user = await User.findOne({ username });
+  if (!user) {
+    return res.status(400).render("login", {
+      pageTitle: "Login",
+      errorMessage: "An account with this username does not exists.",
+    });
+  }
+  const match = await bycrypt.compare(password, user.password);
+  if (!match) {
+    return res.status(400).render("login", {
+      pageTitle: "Login",
+      errorMessage: "Wrong password",
+    });
+  }
+  req.session.loggedIn = true;
+  req.session.user = user;
+  return res.redirect("/");
+};
+
+export const logout = (req, res) => res.send("Log out");
+
+export const edit = (req, res) => res.send("Edit User");
+
+export const deleteUser = (req, res) => res.send("Remove User");
+
+export const see = (req, res) => res.send(`User #${req.params.id}`);
