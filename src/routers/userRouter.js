73198df7e53566b16eebf5017429ced4fb525f1b
MS269 <59796004+MS269@users.noreply.github.com>
2021-05-26 00:10:28 +0900

    updated edit profile

--- src/routers/userRouter.js
+++ src/routers/userRouter.js
@@ -1,7 +1,8 @@
 import express from "express";
 import {
   logout,
-  edit,
+  getEdit,
+  postEdit,
   remove,
   startGithubLogin,
   finishGithubLogin,
@@ -11,7 +12,7 @@ import {
 const userRouter = express.Router();
 
 userRouter.get("/logout", logout);
-userRouter.get("/edit", edit);
+userRouter.route("/edit").get(getEdit).post(postEdit);
 userRouter.get("/remove", remove);
 userRouter.get("/github/start", startGithubLogin);
 userRouter.get("/github/finish", finishGithubLogin);
